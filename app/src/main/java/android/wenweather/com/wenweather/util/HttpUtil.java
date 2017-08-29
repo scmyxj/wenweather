e89dbe428b27c59c2a31c5a21e0d03f3c0c3782d
xiangjin <2685951@qq.com>
2017-08-29 13:57:01 +0800

    db,util,res

---
+++ app/src/main/java/android/wenweather/com/wenweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package android.wenweather.com.wenweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Administrator on 8/28/2017.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
